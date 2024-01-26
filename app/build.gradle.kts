c2ccc70a1984feafe8200dd90237729757b5a891
mixedhash <danandone97@gmail.com>
2024-01-26 18:53:20 +0200

    Removes androidTest package and dependencies + adds JUnit 5 and MockK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,30 +54,49 @@ android {
 }
 
 dependencies {
+    // Core/KTX
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Compose Navigation
     implementation("androidx.navigation:navigation-compose:2.7.6")
+
+    // Work runtime?
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+
+    // Moshi
     implementation("com.squareup.moshi:moshi:1.15.0")
+
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     implementation("androidx.hilt:hilt-navigation-fragment:1.1.0")
-    implementation("com.google.android.gms:play-services-location:21.1.0")
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
-    ksp("androidx.room:room-compiler:2.6.1")
     ksp("com.google.dagger:dagger-compiler:2.50")
     ksp("com.google.dagger:hilt-android-compiler:2.50")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // GMS
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+
+    // Room
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // Compose test
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Testing
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
