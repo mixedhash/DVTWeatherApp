a3250fd42ff3eced66f528bbf5edac5f92cfc948
mixedhash <danandone97@gmail.com>
2024-01-29 00:49:43 +0200

    Removed unused import

--- app/src/main/java/com/silosoft/technologies/dvtweatherapp/data/implementations/LocationRepositoryImpl.kt
+++ app/src/main/java/com/silosoft/technologies/dvtweatherapp/data/implementations/LocationRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.silosoft.technologies.dvtweatherapp.data.implementations
 import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
 import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
